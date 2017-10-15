49e61e9e923690d750d4d3ee07bcd42bd675013c
lmhufnagel <lmhufnagel@gmail.com>
2017-10-15 15:32:02 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i=0;i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/lmhufnagel/repos')
+  req.send()
+}
